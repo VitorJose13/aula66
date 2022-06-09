80c0d641fccd7b3912121eeeaba4db007ab3e9c7
VitorJose13 <vitorsilvaforte3@gmail.com>
2022-06-09 12:06:09 -0300

    Primeiro Commit

---
+++ src/API/useGithubUsers.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+
+
+export default function useGithubUsers() {
+    const [query, setQuery] = useState("cassiano")
+    const [results, setResults] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    // each rerender
+    useEffect(() => {
+        setLoading(true);
+        fetch(`https://api.github.com/search/users?q=${query}`, { method: "GET"}).then(req => {
+            return req.json();
+        }).then(data => {
+            setResults(data.items)
+        })
+    },[query])
+
+    return {
+        query,
+        setQuery,
+        setLoading,
+        results,
+        loading,
+    }
+}
